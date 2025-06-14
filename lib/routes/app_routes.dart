b09c157e8ce75e7d2330f55c099db275599bcd84
Emmanuel kufre <whitemaxwell5@gmail.com>
2025-06-14 02:20:40 +0100

    sal

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,9 @@
+MaterialApp(
+  title: 'Skiibidi',
+  initialRoute: '/welcome',
+  routes: {
+    '/welcome': (context) => const WelcomeScreen()
+    // '/create_wallet': (context) => const CreateWalletScreen(), 
+    // '/import_wallet': (context) => const ImportWalletScreen(), 
+  },
+)dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
